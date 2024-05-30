cc9b4a6d224d9fe2038523e99185a6919e1c2e77
Dompoo <dlckdrms0517@gmail.com>
2024-05-30 16:26:59 +0900

    인그레이트 프로젝트 시작

---
+++ src/main/java/dompoo/Ingrate/IngrateApplication.java
@@ -0,0 +1,13 @@
+package dompoo.Ingrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dompoo/Ingrate/IngrateApplicationTests.java
@@ -0,0 +1,13 @@
+package dompoo.Ingrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
