a7a9e4245dfa74739501e09e822ddc73d960240f
Dompoo <dlckdrms0517@gmail.com>
2024-05-31 23:13:15 +0900

    feat : 식재료 관련 request DTO에 validation 적용

--- src/main/java/dompoo/Ingrate/ingredient/dto/IngredientEditRequest.java
+++ src/main/java/dompoo/Ingrate/ingredient/dto/IngredientEditRequest.java
@@ -1,5 +1,8 @@
 package dompoo.Ingrate.ingredient.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,9 +12,21 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class IngredientEditRequest {
 
+    @NotNull(message = "식재료명을 입력해주세요.")
+    @NotEmpty(message = "식재료명은 비어있을 수 없습니다.")
     private String name;
+
+    @NotNull(message = "가격을 입력해주세요.")
+    @Positive(message = "가격은 0보다 커야합니다.")
     private Float cost;
+
+    @NotNull(message = "양을 입력해주세요.")
+    @Positive(message = "양은 0보다 커야합니다.")
     private Float amount;
+
+    @NotNull(message = "단위를 입력해주세요.")
+    @NotEmpty(message = "단위는 비어있을 수 없습니다.")
     private String unit;
+
     private String memo;
 }
