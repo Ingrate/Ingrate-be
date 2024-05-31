06606597ba3ffebbb07db7507b5dc775157e7766
Dompoo <dlckdrms0517@gmail.com>
2024-06-01 02:49:53 +0900

    feat : 로그인 성공 핸들러, security 설정

---
+++ src/main/java/dompoo/Ingrate/config/LoginSuccessHandler.java
@@ -0,0 +1,41 @@
+package dompoo.Ingrate.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_OK;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+@RequiredArgsConstructor
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("role", principal.getRole());
+        data.put("username", principal.getUsername());
+        data.put("memberId", principal.getMemberId());
+        String json = objectMapper.writeValueAsString(data);
+
+        log.info("[인증 성공] 사용자명 : {}", principal.getUsername());
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.getWriter().write(json);
+        response.setCharacterEncoding(UTF_8.name());
+        response.setStatus(SC_OK);
+    }
+}
--- src/main/java/dompoo/Ingrate/config/SecurityConfig.java
+++ src/main/java/dompoo/Ingrate/config/SecurityConfig.java
@@ -1,16 +1,97 @@
 package dompoo.Ingrate.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dompoo.Ingrate.member.Member;
+import dompoo.Ingrate.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
+
+import static org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/favicon.ico").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/auth/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(SAMEORIGIN)))
+                .addFilterAt(jsonUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling(e -> e TODO: 예외 핸들러 작성
+//                        .accessDeniedHandler(new Http403Handler(objectMapper))
+//                        .authenticationEntryPoint(new Http401Handler(objectMapper)))
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .deleteCookies("SESSION")
+//                        .logoutSuccessHandler(new LogoutSuccessHandler()) TODO: 로그아웃 성공 핸들러 작성
+                )
+                .build();
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() {
+        JsonUsernamePasswordAuthenticationFilter filter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setFilterProcessesUrl("/auth/login");
+        //filter.setAuthenticationFailureHandler(new LoginFailHandler(objectMapper)); TODO: 로그인 실패 핸들러 작성
+        filter.setAuthenticationSuccessHandler(new LoginSuccessHandler(objectMapper));
+        filter.setSecurityContextRepository(new HttpSessionSecurityContextRepository());
+
+        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+        rememberMeServices.setAlwaysRemember(true);
+        rememberMeServices.setValiditySeconds(2592000);
+        filter.setRememberMeServices(rememberMeServices);
+
+        return filter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService(memberRepository));
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(MemberRepository memberRepository) {
+        return username -> {
+            Member member = memberRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException(username + "을 찾을 수 없습니다."));
+
+            return new UserPrincipal(member);
+        };
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
