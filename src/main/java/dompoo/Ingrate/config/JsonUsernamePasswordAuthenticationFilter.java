efc8fcf209cb61daf17dccb6767122237c88e471
Dompoo <dlckdrms0517@gmail.com>
2024-06-01 02:44:03 +0900

    feat : Principal, AuthFilter 작성

---
+++ src/main/java/dompoo/Ingrate/config/JsonUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,37 @@
+package dompoo.Ingrate.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        UsernamePassword json = objectMapper.readValue(request.getInputStream(), UsernamePassword.class);
+
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(json.username, json.password);
+
+        setDetails(request, token);
+
+        return this.getAuthenticationManager().authenticate(token);
+    }
+
+    @Getter
+    private static class UsernamePassword {
+        private String username;
+        private String password;
+    }
+}
---
+++ src/main/java/dompoo/Ingrate/config/UserPrincipal.java
@@ -0,0 +1,26 @@
+package dompoo.Ingrate.config;
+
+import dompoo.Ingrate.member.Member;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Getter
+public class UserPrincipal extends User {
+
+    private final Long memberId;
+    private final String username;
+    private final String role;
+
+    public UserPrincipal(Member member) {
+        super(member.getUsername(),
+                member.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        this.memberId = member.getId();
+        this.username = member.getUsername();
+        this.role = member.getRole().getName();
+    }
+}
