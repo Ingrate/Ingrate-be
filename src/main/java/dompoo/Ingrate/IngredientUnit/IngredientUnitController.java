60175b456f48690b78e095178f68fcad75d1ec3f
Dompoo <dlckdrms0517@gmail.com>
2024-06-08 17:29:08 +0900

    feat : 식재료-단위 컨트롤러에 어드민 관련 메서드 시큐리티 적용

--- src/main/java/dompoo/Ingrate/IngredientUnit/IngredientUnitController.java
+++ src/main/java/dompoo/Ingrate/IngredientUnit/IngredientUnitController.java
@@ -3,6 +3,7 @@ package dompoo.Ingrate.IngredientUnit;
 import dompoo.Ingrate.IngredientUnit.dto.UnitAddRequest;
 import dompoo.Ingrate.IngredientUnit.dto.UnitResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ public class IngredientUnitController {
     }
 
     //어드민 기능
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/ingredient/unit")
     public UnitResponse addUnit(@RequestBody UnitAddRequest request) {
         return ingredientUnitService.addUnit(request);
