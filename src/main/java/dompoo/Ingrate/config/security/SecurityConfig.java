1ddd571eff3915425a51a128ee24d32e0b9cda6e
Dompoo <dlckdrms0517@gmail.com>
2024-06-08 17:27:16 +0900

    feat : 메서드 시큐리티 설정

--- src/main/java/dompoo/Ingrate/config/security/SecurityConfig.java
+++ src/main/java/dompoo/Ingrate/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,6 +28,7 @@ import static org.springframework.security.web.header.writers.frameoptions.XFram
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
