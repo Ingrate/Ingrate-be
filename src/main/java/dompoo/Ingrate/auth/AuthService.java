62a07d71e747df0d7ea6f6858e199e3f144d1ab1
Dompoo <dlckdrms0517@gmail.com>
2024-07-07 16:41:00 +0900

    feat : Auth 컨트롤러 / 서비스 작성

---
+++ src/main/java/dompoo/Ingrate/auth/AuthService.java
@@ -0,0 +1,66 @@
+package dompoo.Ingrate.auth;
+
+import dompoo.Ingrate.auth.dto.LoginRequest;
+import dompoo.Ingrate.auth.dto.LoginResponse;
+import dompoo.Ingrate.config.security.JwtTokenUtil;
+import dompoo.Ingrate.exception.AlreadyExistUsername;
+import dompoo.Ingrate.exception.PasswordCheckIncorrect;
+import dompoo.Ingrate.exception.PasswordIncorrect;
+import dompoo.Ingrate.exception.UsernameNotFoundException;
+import dompoo.Ingrate.member.Member;
+import dompoo.Ingrate.member.MemberRepository;
+import dompoo.Ingrate.member.dto.SignUpRequest;
+import dompoo.Ingrate.member.dto.SignUpResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class AuthService {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder encoder;
+
+    public SignUpResponse signUp(SignUpRequest signUpRequest) {
+        if (memberRepository.existsByUsername(signUpRequest.getUsername())) {
+            throw new AlreadyExistUsername();
+        }
+
+        if (!signUpRequest.getPassword().equals(signUpRequest.getPasswordCheck())) {
+            throw new PasswordCheckIncorrect();
+        }
+
+        Member member = memberRepository.save(Member.builder()
+                .username(signUpRequest.getUsername())
+                .password(encoder.encode(signUpRequest.getPassword()))
+                .build());
+
+        return new SignUpResponse(member);
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        Member member = memberRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException(loginRequest.getUsername()));
+
+        //비밀번호가 틀리면
+        if (!encoder.matches(loginRequest.getPassword(), member.getPassword())) {
+            throw new PasswordIncorrect();
+        }
+
+        return LoginResponse
+                .builder()
+                .id(member.getId())
+                .username(member.getUsername())
+                .accessToken(jwtTokenUtil.generateToken(member.getUsername()))
+                .build();
+    }
+
+}
