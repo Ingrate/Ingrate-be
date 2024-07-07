c77778637b45dce07c853a8503ffefce3950588f
Dompoo <dlckdrms0517@gmail.com>
2024-07-07 21:18:59 +0900

    chore : AuthenticationFail 예외 메시지 수정

--- src/main/java/dompoo/Ingrate/exception/AuthenticationFail.java
+++ src/main/java/dompoo/Ingrate/exception/AuthenticationFail.java
@@ -4,7 +4,7 @@ import dompoo.Ingrate.config.exception.MyException;
 
 public class AuthenticationFail extends MyException {
 
-    private static final String MESSAGE = "아이디나 비밀번호가 올바르지 않습니다.";
+    private static final String MESSAGE = "아이디 혹은 비밀번호가 올바르지 않습니다.";
     private static final String CODE = "400";
 
     public AuthenticationFail() {
