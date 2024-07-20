9a8a086d0036874958b13db28192545ffd22cc79
Dompoo <dlckdrms0517@gmail.com>
2024-07-21 00:23:27 +0900

    feat : 각 엔티티의 칼럼의 옵션을 구체적으로 작성

--- src/main/java/dompoo/Ingrate/ingredient/Ingredient.java
+++ src/main/java/dompoo/Ingrate/ingredient/Ingredient.java
@@ -16,11 +16,24 @@ public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false)
     private String name;
+    
+    @Column(nullable = false)
     private Float cost;
+    
+    @Column(nullable = false)
     private Float amount;
+    
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
     private Unit unit;
+    
+    @Column(nullable = false)
     private String memo;
+    
+    @Column(nullable = false)
     private LocalDate date;
 
     @ManyToOne
